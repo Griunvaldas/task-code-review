8a41c63dfc12f911ca226a1542c9af5ed2a38c9d
Karolis Bartkus <karolis@bartkus.eu>
2023-12-26 20:51:02 +0200

    code explaining and little more beautification

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -11,7 +11,6 @@ use App\Service\Sender\SenderResolver;
 use App\System\Customer\CustomerMessageBuilder;
 use App\System\SerializerBuilder;
 use Doctrine\Persistence\ObjectRepository;
-use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,11 +20,10 @@ use Symfony\Component\Serializer\Exception\MissingConstructorArgumentsException;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-/**
- *
- */
 class CustomerController extends AbstractController
 {
+    private const PAYLOAD_TYPE = 'json';
+
     private ValidatorInterface $validator;
 
     public function __construct(ValidatorInterface $validator)
@@ -34,59 +32,62 @@ class CustomerController extends AbstractController
     }
 
     /**
-     *
      * @Route("/customer/{code}/notifications", name="customer_notifications", methods={"GET"})
      */
     public function notifyCustomer(string $code, Request $request): JsonResponse
     {
         try {
+            // we try to get customer entity from ORM
             $customer = $this->getCustomerRepository()->findOneBy(['code' => $code]);
 
+            // if customer doesn't exist there is no point for further code execution, so we return 404
             if (!$customer instanceof Customer) {
-                return new JsonResponse(
-                    ['success' => 0, 'customer' => 'not found'],
-                    Response::HTTP_NOT_FOUND
-                );
+                return $this->jsonError(['customer' => 'not found'], Response::HTTP_NOT_FOUND);
             }
 
-            if (!empty($request->getContent())) {
-                $requestData = SerializerBuilder::build()->deserialize(
-                    $request->getContent(),
-                    NotifyCustomerDto::class,
-                    'json'
-                );
-
-                $validationResult = $this->validate($requestData);
-
-                if (!empty($validationResult)) {
-                    return new JsonResponse(
-                        ['success' => 0, 'errors' => $validationResult],
-                        Response::HTTP_UNPROCESSABLE_ENTITY
-                    );
-                }
-            } else {
-                return new JsonResponse(['success' => 0], Response::HTTP_BAD_REQUEST);
-            }
-
-            $success = 0;
-
-            if ($customer->getNotificationType() === $requestData->getType()) {
-                $message = $this->getMessageBuilder()->build($requestData, $customer);
-                $messenger = new Messenger(SenderResolver::resolve($customer->getNotificationType()));
-                $messenger->send($message);
-                $success = (int)$messenger->isSent();
-            }
-
-            return new JsonResponse(
-                [
-                    'success' => $success,
-                    'message' => [
-                        'type' => $requestData->getType(),
-                        'body' => $requestData->getBody(),
-                    ]
-                ]
+            // using Symfony serializer component to deserialize provided JSON to DTO
+            $requestData = SerializerBuilder::build()->deserialize(
+                $request->getContent(),
+                NotifyCustomerDto::class,
+                self::PAYLOAD_TYPE
             );
+
+            // and use Symfony validator component to check if received data meets our expectations
+            $validationErrors = $this->validate($requestData);
+
+            // if there is anything - return them with 422
+            if (!empty($validationErrors)) {
+                return $this->jsonError($validationErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            // we may want to check if customer notificationType matches message type - customer may not want to get
+            // SMS if he opted for email, and vice versa :)
+            if ($customer->getNotificationType() === $requestData->getType()) {
+                // we resolve Sender by customer-provided notification type, and then initialize Messenger
+                // with appropiate sender only
+                $sender = SenderResolver::resolve($customer->getNotificationType());
+                $messenger = new Messenger($sender);
+
+                // we build and send message for customer
+                $messenger->send(
+                    $this->getMessageBuilder()->build($requestData, $customer)
+                );
+
+                // even then, if something wrong happens in code initialization or validation, we check additionally
+                // if message has been sent and provide that to our returned payload
+                $success = (int)$messenger->isSent();
+            } else {
+                $success = 0;
+            }
+
+            // final output of 200 and results - type and body
+            return $this->jsonSuccess($success, [
+                'type' => $requestData->getType(),
+                'body' => $requestData->getBody(),
+            ]);
         } catch (MissingConstructorArgumentsException|\InvalidArgumentException $e) {
+            // we can log exception to sentry or any other monitoring/logs service
+            // end-user should not see exceptions so we return bad request instead
             return new JsonResponse(['success' => 0], Response::HTTP_BAD_REQUEST);
         }
     }
@@ -100,6 +101,7 @@ class CustomerController extends AbstractController
 
     private function mapErrors(ConstraintViolationListInterface $errors): array
     {
+        // KISS - done with foreach on purpose for easy readability
         $violations = [];
 
         foreach ($errors as $error) {
@@ -117,5 +119,28 @@ class CustomerController extends AbstractController
     private function getMessageBuilder(): CustomerMessageBuilder
     {
         return new CustomerMessageBuilder();
+    }
+
+
+    // helper methods to prevent lots of duplicated code in parent method
+    private function jsonError(array $error, int $code): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'success' => 0,
+                'error' => $error
+            ],
+            $code
+        );
+    }
+
+    private function jsonSuccess(int $success, array $data = []): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'success' => $success,
+                'message' => $data
+            ]
+        );
     }
 }
